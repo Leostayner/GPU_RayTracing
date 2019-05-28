17d1a89284a96ef95ebf435def714e82abe52e2c
leostayner <leostayner@ip-172-31-34-210.ec2.internal>
2019-05-28 17:29:14 +0000

    capitulo2: Adding vec3

--- main.cu
+++ main.cu
@@ -1,14 +1,13 @@
 #include <iostream>
+#include "vec3.h"
 
-__global__ void render(float *img, int nx, int ny) {
+__global__ void render(vec3 *img, int nx, int ny) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= nx) || (j >= ny)) return;
     
-    int pixel_index = j*nx*3 + i*3;
-    img[pixel_index + 0] = float(i) / nx;
-    img[pixel_index + 1] = float(j) / ny;
-    img[pixel_index + 2] = 0.2;
+    int pixel_index = j*nx + i;
+    img[pixel_index] = vec3( float(i)/nx, float(j)/ny, 0.2f);
 }
 
 int main() {
@@ -20,9 +19,9 @@ int main() {
     std::cout << "Rendering Image: " << nx << "x" << ny << std::endl;
 
     int num_pixels = nx*ny;
-    size_t img_size = 3*num_pixels*sizeof(float);
+    size_t img_size = num_pixels*sizeof(vec3);
 
-    float *img;
+    vec3 *img;
     cudaMallocManaged((void **)&img, img_size);
 
     dim3 blocks(nx/tx+1,ny/ty+1);
@@ -35,14 +34,10 @@ int main() {
     
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            size_t pixel_index = j*3*nx + i*3;
-            float r = img[pixel_index + 0];
-            float g = img[pixel_index + 1];
-            float b = img[pixel_index + 2];
-            
-            int ir = int(255.99*r);
-            int ig = int(255.99*g);
-            int ib = int(255.99*b);
+            size_t pixel_index = j*nx + i;
+            int ir = int(255.99*img[pixel_index].r());
+            int ig = int(255.99*img[pixel_index].g());
+            int ib = int(255.99*img[pixel_index].b());
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
